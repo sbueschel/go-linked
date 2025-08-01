14131877e919f23689d4a7ee154d98d872da90e7
Shane Bueschel <shane@hyperbit.io>
2025-08-01 12:33:04 -0500

    Update test functions used

--- edge_test.go
+++ edge_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/sbueschel/go-linked"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestEdge(t *testing.T) {
@@ -56,46 +55,46 @@ func TestEdge(t *testing.T) {
 	} {
 		t.Run(tc.Name, func(t *testing.T) {
 			t.Run("IsValid", func(t *testing.T) {
-				assert.Equal(t, tc.ExpectIsValid, tc.Edge.IsValid())
+				Assert(t, tc.ExpectIsValid, tc.Edge.IsValid())
 			})
 
 			t.Run("String", func(t *testing.T) {
-				assert.Equal(t, tc.ExpectString, tc.Edge.String())
+				Assert(t, tc.ExpectString, tc.Edge.String())
 			})
 
 			t.Run("Suffix", func(t *testing.T) {
-				assert.Equal(t, tc.ExpectSuffix, tc.Edge.Suffix())
+				Assert(t, tc.ExpectSuffix, tc.Edge.Suffix())
 			})
 
 			t.Run("List", func(t *testing.T) {
-				assert.Equal(t, tc.ExpectList, tc.Edge.List())
+				Assert(t, tc.ExpectList, tc.Edge.List())
 			})
 
 			t.Run("Node", func(t *testing.T) {
-				assert.Equal(t, tc.ExpectNode, tc.Edge.Node())
+				Assert(t, tc.ExpectNode, tc.Edge.Node())
 			})
 
 			t.Run("Must", func(t *testing.T) {
 				if tc.ExpectIsValid {
-					assert.NotPanics(t, func() {
-						assert.Equal(t, tc.Edge, tc.Edge.Must())
+					AssertNoPanic(t, func() {
+						Assert(t, tc.Edge, tc.Edge.Must())
 					})
 				} else {
-					assert.Panics(t, func() { _ = tc.Edge.Must() })
+					AssertPanic(t, func() { _ = tc.Edge.Must() })
 				}
 			})
 
 			t.Run("Opposite", func(t *testing.T) {
 				var expect linked.Edge
 				if !tc.ExpectIsValid {
-					assert.Panics(t, func() { _ = tc.Edge.Opposite() })
+					AssertPanic(t, func() { _ = tc.Edge.Opposite() })
 					return
 				} else if tc.Edge == linked.Left {
 					expect = linked.Right
 				}
 
-				assert.NotPanics(t, func() {
-					assert.Equal(t, expect, tc.Edge.Opposite())
+				AssertNoPanic(t, func() {
+					Assert(t, expect, tc.Edge.Opposite())
 				})
 			})
 		})
